6c2fe927472f18a966add46df4a0162ab014448c
Artyom Golubev <artgolubev@bk.ru>
2022-11-13 15:27:47 +0300

    main functional is ready

--- w_app1/views.py
+++ w_app1/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 # Create your views here.
 
 def index(request):
-    city = 'London'
-    country = 'uk'
-    #url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API_key}'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c9246b554713d21f42bb2b916c52ab20'
 
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric&appid=c9246b554713d21f42bb2b916c52ab20'
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)           # создаем объект на основе класса, который содержит форму
+        form.save()                     # сохраняем данные в БД
 
-    res = requests.get(url.format(city,country)).json()
+    form = CityForm                     # очистим форму при перезагрузке страницы
 
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon']
-    }
-    context = {'info': city_info}
+    cities = City.objects.all()                     # обращаемся к таблице City, выбираем из нее все данные
+    all_cities = []
+    for city in cities:                                         # перебираем полученные данные
+        res = requests.get(url.format(city.name)).json()        # узнаем погоду для каждого города
+        city_info = {                                           # записываем нужные данные в словарь
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon']
+        }
+        all_cities.append(city_info)                     # записываем словарь в список
+
+    context = {'all_info': all_cities, 'form': form}     # передаем список и форму в html-шаблон
 
     return render(request, 'weather/index.html', context)
