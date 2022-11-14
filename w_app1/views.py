eaaa1469a8e79a7dae5353449e49bae3d326fa70
Artyom Golubev <artgolubev@bk.ru>
2022-11-14 22:38:55 +0300

    nothing change

--- w_app1/views.py
+++ w_app1/views.py
@@ -9,12 +9,12 @@ def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c9246b554713d21f42bb2b916c52ab20'
 
     if(request.method == 'POST'):
-        form = CityForm(request.POST)           # создаем объект на основе класса, который содержит форму
+        form = CityForm(request.POST)   # создаем объект на основе класса, который содержит форму
         form.save()                     # сохраняем данные в БД
 
     form = CityForm                     # очистим форму при перезагрузке страницы
 
-    cities = City.objects.all()                     # обращаемся к таблице City, выбираем из нее все данные
+    cities = City.objects.all()         # обращаемся к таблице City, выбираем из нее все данные
     all_cities = []
     for city in cities:                                         # перебираем полученные данные
         res = requests.get(url.format(city.name)).json()        # узнаем погоду для каждого города
@@ -26,5 +26,4 @@ def index(request):
         all_cities.append(city_info)                     # записываем словарь в список
 
     context = {'all_info': all_cities, 'form': form}     # передаем список и форму в html-шаблон
-
     return render(request, 'weather/index.html', context)
